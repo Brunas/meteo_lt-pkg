cee0c69546278a9d3744eaaccf2a6776630a04e5
Ramūnas Bunokas <Ramunas.Bunokas@oag.com>
2024-07-26 06:51:48 +0000

    Added dependencies, requirements, readme, pipeline update

--- meteo_lt/__init__.py
+++ meteo_lt/__init__.py
@@ -4,8 +4,3 @@ from .api import MeteoLtAPI
 from .models import Coordinates, Place, ForecastTimestamp, Forecast
 
 __all__ = ["MeteoLtAPI", "Coordinates", "Place", "ForecastTimestamp", "Forecast"]
-
-__title__ = "Meteo.Lt"
-__version__ = "0.1.6"
-__author__ = "Brunas"
-__license__ = "MIT"
